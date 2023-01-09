4570078a31d6fa445757fc23f153dc66e7285e7b
Waltteri Miikkulainen <waltteri.miikkulainen@student.careeria.fi>
2023-01-09 13:09:50 +0200

    Harjoitus2

---
+++ Olio ohjelmointi/Harjotus2/Opiskelija.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjotus2
+{
+    internal class Opiskelija
+    {
+        public string Nimi;
+        public string OpiskelijaID;
+        public int opintopisteet;
+
+        public Opiskelija(string nimi, string opiskelijaID, int opintopisteet)
+        {
+            Nimi = nimi;
+            OpiskelijaID = opiskelijaID;
+            this.opintopisteet = opintopisteet;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("opiskelian nimi on " + Nimi);
+            Console.WriteLine("opiskelian id on " + OpiskelijaID);
+            Console.WriteLine("opiskelian oi on " + opintopisteet);
+        }
+        public void Muokaaopintopisteitä(int määrä) 
+        {
+            opintopisteet += määrä;
+        }
+    }
+}
---
+++ Olio ohjelmointi/Harjotus2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjotus2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        public string Nimi;
+        public string OpiskelijaID;
+        public int opintopisteet;
+
+        public Program(string nimi, string opiskelijaID, int opintopisteet)
+        {
+            Nimi = nimi;
+            OpiskelijaID = opiskelijaID;
+            this.opintopisteet = opintopisteet;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("opiskelian nimi on " + Nimi);
+            Console.WriteLine("opiskelian id on " + OpiskelijaID);
+            Console.WriteLine("opiskelian opintopisteet on " + opintopisteet);
+        }
+        
+    }
+    }
+}
