fb619c02d6c7e26d250de5a8f8c7139f903eb378
Waltteri Miikkulainen <waltteri.miikkulainen@student.careeria.fi>
2023-01-09 13:14:56 +0200

    Harjotus2 päivitys

--- Olio ohjelmointi/Harjotus3/Program.cs
+++ Olio ohjelmointi/Harjotus3/Program.cs
@@ -4,16 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Harjotus2
+namespace Harjotus3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Opiskelija opiskelia1 = new Opiskelija("pertti", "careeria",180);
-            opiskelia1.TulostaData();
-            opiskelia1.Muokaaopintopisteitä(5);
-            opiskelia1.TulostaData();
+
         }
     }
 }
