92f2e0e81d386fcf673525e1fd34d0edbdc3cb2e
Waltteri Miikkulainen <waltteri.miikkulainen@student.careeria.fi>
2023-01-09 12:21:32 +0200

    olioharjoteluu

---
+++ Olio ohjelmointi/Olio ohjelmointi/Koira.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_ohjelmointi
+{
+    internal class Koira
+    {
+        public string nimi;
+        public string Rotu;
+        public int Ikä;
+
+        public void HaeTiedot()
+        {
+            Console.WriteLine("Koiran nimi on" + nimi);
+            Console.WriteLine("Koiran ikä on" + Ikä);
+            Console.WriteLine("Koiran Rotu on" + Rotu);
+
+
+        }
+
+        public void Hauku()
+        {
+            Console.WriteLine(" woof woof");
+        }
+          
+    }
+}
--- Olio ohjelmointi/Olio ohjelmointi/Program.cs
+++ Olio ohjelmointi/Olio ohjelmointi/Program.cs
@@ -10,6 +10,23 @@ namespace Olio_ohjelmointi
     {
         static void Main(string[] args)
         {
+            Koira koira1 = new Koira("Rekku" , "mopsi"); // luodaaan uusi koira olio/instanssi
+            koira1.nimi = "RekkuRekku";
+            koira1.Ikä = 2;
+            koira1.Rotu = "Mopsi";
+
+            koira1.HaeTiedot();
+            
+
+            Koira koira2= new Koira("ali" , "bulldog");
+            koira2.nimi = "ali";
+            koira2.Ikä = 5;
+            koira2.Rotu = "bulldog";
+
+            koira2.HaeTiedot();
+
+            koira1.Hauku();
+            koira2.Hauku();
 
         }
     }
