4df828cdbc12e0810317dee49372fc73f7da1025
Waltteri Miikkulainen <waltteri.miikkulainen@student.careeria.fi>
2023-01-10 10:48:11 +0200

    Harjotus5

---
+++ Olio ohjelmointi/Harjootus 5/Laskin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjootus_5
+{
+    static class Laskin
+    {
+        public static float summa(float a,float b) 
+        {
+            return a + b; 
+        }
+        public static  float erotus(float a, float b) 
+        {
+            return a - b;
+
+        }
+        public static  float kertolasku(float a, float b) 
+        {
+            return a * b;
+
+        }
+        public static  float jako(float a, float b) 
+        {
+            return a / b;
+        }
+
+    }
+}
