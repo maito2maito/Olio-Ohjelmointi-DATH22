ed3bf3fcdd8190a9fe72ec3b57a0be8cffea4338
Waltteri Miikkulainen <waltteri.miikkulainen@student.careeria.fi>
2023-01-13 10:24:14 +0200

    Harjoitus7

---
+++ Olio ohjelmointi/HArjoitus 7/Hissi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HArjoitus_7
+{
+    internal class Hissi
+    {
+        public int nykyinenKerros;
+        public int minKerros;
+        public int maxKerros;
+
+        public Hissi(int nykyinenKerros, int minKerros, int maxKerros)
+        {
+            this.nykyinenKerros = nykyinenKerros;
+            this.minKerros = minKerros;
+            this.maxKerros = maxKerros;
+        }
+        
+
+        public void VaihdaKerrosta(int uusikerros )
+        {
+            nykyinenKerros = uusikerros;
+            if (nykyinenKerros >maxKerros)
+            {
+               nykyinenKerros = maxKerros;
+            }
+            if (nykyinenKerros< minKerros)
+            {
+                nykyinenKerros = minKerros;
+            }
+
+            Console.WriteLine("hissi on kerrokseta" + nykyinenKerros);
+        } 
+
+
+
+    }
+}
---
+++ Olio ohjelmointi/HArjoitus 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HArjoitus_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hissi hissi = new Hissi(1,1, 7);
+            hissi.VaihdaKerrosta(2);
+            hissi.VaihdaKerrosta(-500);
+            hissi.VaihdaKerrosta(-100);
+            hissi.VaihdaKerrosta(5);
+            hissi.VaihdaKerrosta(10);
+                
+
+           
+            
+          
+
+
+        }
+    }
+}
