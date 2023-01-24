b8a0128b4a6aa3c7eed2f6250f7530e95d3097c4
Waltteri Miikkulainen <waltteri.miikkulainen@student.careeria.fi>
2023-01-24 10:01:08 +0200

    kiuuas WPF

---
+++ Olio ohjelmointi/kiuas2 WPF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kiuas2_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            slider_Kosteus.IsEnabled = false;
+            slider_lämpötila.IsEnabled = false;
+        }
+
+        private void ckb1_Checked(object sender, RoutedEventArgs e)
+        {
+            slider_Kosteus.IsEnabled = true;
+            slider_lämpötila.IsEnabled = true;
+
+        }
+
+
+
+        private void slider_Kosteus_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txb_k.Text = "Kosteus:" + slider_Kosteus.Value;
+        }
+
+        private void slider_lämpötila_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txt_L.Text = "Lämpötila:" + slider_lämpötila.Value;
+        }
+
+        private void ckb1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            slider_Kosteus.IsEnabled = false;
+            slider_lämpötila.IsEnabled = false;
+
+        }
+
+
+    }
+}
+
---
+++ Olio ohjelmointi/wpf harjoitus auto/Ajoneuvo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_harjoitus_auto
+{
+    internal class Ajoneuvo
+    {
+        public int renkaat;
+        public int nopeus;
+        public string merkki;
+
+        public Ajoneuvo(int renkaat, int nopeus, string merkki)
+        {
+            this.renkaat = renkaat;
+            this.nopeus = nopeus;
+            this.merkki = merkki;
+        }
+    }
+}
